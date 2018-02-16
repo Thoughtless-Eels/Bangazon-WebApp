2675172cc7f0af7ee3012fe202c9e9d2f90ec154
Leah <leaheduvic@gmail.com>
2018-02-16 13:50:02 -0600

    order model

--- BangazonWebApp/Models/Order.cs
+++ BangazonWebApp/Models/Order.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BangazonWebApp.Models
 {
-    public class Orders
+    public class Order
     {
+        [Key]
+        public int OrderId { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime DateCreated { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateCompleted { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+
+        //add PaymentType constructor and ICollection for Items//
     }
 }
