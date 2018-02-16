b6bcb13219717056b9cab3827f6aae0b5ca2c03d
Leah <leaheduvic@gmail.com>
2018-02-16 14:39:48 -0600

    line item model

--- BangazonWebApp/Models/LineItem.cs
+++ BangazonWebApp/Models/LineItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,17 @@ namespace BangazonWebApp.Models
 {
     public class LineItem
     {
+        [Key]
+        public int LineItemId { get; set; }
+
+        [Required]
+        public int OrderId { get; set; }
+
+        public Order Order { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        public Product Product { get; set; }
     }
 }
