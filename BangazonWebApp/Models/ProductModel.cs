0c29c6f93c77634480b751c57483d6ce14b0fe7a
Garrett Ward <gww8804@gmail.com>
2018-02-16 11:39:41 -0800

    PRAWDUCT MAWDEL

---
+++ BangazonWebApp/Models/ProductModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWebApp.Models
+{
+    public class ProductModel
+    {
+
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Description { get; set; }
+
+        [Required]
+        [StringLength(55, ErrorMessage = "Please shorten the product title to 55 characters")]
+        public string Title { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        public ApplicationUser User { get; set; }
+
+        [Required]
+        [Display(Name = "Product Category")]
+        public int ProductTypeId { get; set; }
+
+        public ProductType ProductType { get; set; }
+
+    }
+}
+
+
