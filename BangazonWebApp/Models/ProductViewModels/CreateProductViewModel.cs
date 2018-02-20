c918d86dcf23a2f7ea09ebabe05c1bc3402d13b6
Garrett Ward <gww8804@gmail.com>
2018-02-20 09:53:06 -0800

    working on implementing view model for product creation

---
+++ BangazonWebApp/Models/ProductViewModels/CreateProductViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BangazonWebApp.Data;
+using BangazonWebApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Bangazon.Models.ProductViewModels
+{
+    public class ProductCreateViewModel
+    {
+        public List<SelectListItem> ProductTypes { get; set; }
+        public Product Product { get; set; }
+
+        public string UserRole { get; set; }
+        public ProductCreateViewModel(ApplicationDbContext ctx)
+        {
+
+            this.ProductTypes = ctx.ProductType
+                                    .OrderBy(l => l.ProductTypeName)
+                                    .AsEnumerable()
+                                    .Select(li => new SelectListItem
+                                    {
+                                        Text = li.ProductTypeName,
+                                        Value = li.ProductTypeId.ToString()
+                                    }).ToList();
+
+            this.ProductTypes.Insert(0, new SelectListItem
+            {
+                Text = "Choose category...",
+                Value = "0"
+            });
+        }
+    }
+}
